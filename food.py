c7716e4c7d8de25ded631f5c8c16135c3b151350
Tae Young Lee <taeleeswe@gmail.com>
2024-04-18 10:07:08 -0400

    third commit

--- food.py
+++ food.py
@@ -5,13 +5,16 @@ import random
 class Food(Turtle):
 
     # instead of setting attributes like self.food = Turtle(), set up Class Inheritance
-    def __int__(self):
+    def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
--- snake.py
+++ snake.py
@@ -17,11 +17,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         # range(start= len(segments) - 1, stop = 0, step = -1)
